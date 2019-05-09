78c9e1364c5d48e4ab884fa3c26a54ac774bd977
Mark Evans <mg17444@essex.ac.uk>
2019-05-09 01:18:47 +0100

    Init commit

---
+++ src/renderEngine/DisplayManager.java
@@ -0,0 +1,37 @@
+package renderEngine;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.*;
+
+public class DisplayManager {
+    private static final int WIDTH = 1280;
+    private static final int HEIGHT = 720;
+    private static final int FPS_CAP = 120;
+    private static boolean flag = false;
+
+    public static void createDisplay() {
+
+        ContextAttribs attribs = new ContextAttribs(3, 2);
+        attribs.withForwardCompatible(true);
+        attribs.withProfileCore(true);
+
+        try {
+            Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+            Display.create(new PixelFormat(), attribs);
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+        }
+//        Set the x,y for the bottom left and top right of the view port
+        GL11.glViewport(0, 0, WIDTH, HEIGHT);
+    }
+
+    public static void updateDisplay() {
+        Display.sync(FPS_CAP);
+        Display.update();
+
+    }
+
+    public static void closeDisplay() {
+        Display.destroy();
+    }
+}
